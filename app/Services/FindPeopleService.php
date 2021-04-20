f9b948439a2ce858aa812006dfd43cf4a822e7fd
JanisKaucis <janiskaucis@gmail.com>
2021-04-20 13:55:32 +0300

    doesnt show same persons if already liked or disliked, works for all users

--- app/Services/FindPeopleService.php
+++ app/Services/FindPeopleService.php
@@ -18,84 +18,103 @@ class FindPeopleService
 
     public function showOppositeSex()
     {
-            $user = $this->registeredUsersRepository->selectByEmail($_SESSION['login']['email']);
-            $userEmail = $user[0]['email'];
-            $date = date('Y');
+        $user = $this->registeredUsersRepository->selectByEmail($_SESSION['login']['email']);
+        $userEmail = $user[0]['email'];
+        $date = date('Y');
 
-            if ($user[0]['gender'] == 'male') {
-                $gender = 'female';
-            } else {
-                $gender = 'male';
-            }
+        if ($user[0]['gender'] == 'male') {
+            $gender = 'female';
+        } else {
+            $gender = 'male';
+        }
         $likedPersonString = file_get_contents('Storage/likedPersons.json');
         $likedPersonArray = json_decode($likedPersonString, true);
-
+        $dislikedPersonString = file_get_contents('Storage/dislikedPersons.json');
+        $dislikedPersonArray = json_decode($dislikedPersonString, true);
         if (isset($_POST['findPeople'])) {
+            file_put_contents('Storage/persons.json','');
+
             $oppositeUsers = $this->registeredUsersRepository->selectByGender($gender);
             shuffle($oppositeUsers);
             if (!empty($likedPersonArray)) {
-                foreach ($likedPersonArray as $likedUsers)
-                    foreach ($oppositeUsers as $oppositeUser) {
-                        {
-                            if ($oppositeUser['email'] == $likedUsers['likedUsers']['email']) {
-                                unset($oppositeUser);
+                foreach ($likedPersonArray as $likedUser) {
+                    if ($userEmail == $likedUser['userEmail']) {
+                        foreach ($oppositeUsers as $key => $oppositeUser) {
+                            if ($oppositeUser['email'] == $likedUser['likedUsers']['email']) {
+                                unset($oppositeUsers[$key]);
                             }
                         }
                     }
+                }
             }
-            $file = fopen('Storage/persons.json', 'w');
-            fwrite($file, json_encode($oppositeUsers));
-            fclose($file);
+            if (!empty($dislikedPersonArray)) {
+                foreach ($dislikedPersonArray as $dislikedUser) {
+                    if ($userEmail == $dislikedUser['userEmail']) {
+                        foreach ($oppositeUsers as $key => $oppositeUser) {
+                            if ($oppositeUser['email'] == $dislikedUser['dislikedUsers']['email']) {
+                                unset($oppositeUsers[$key]);
+                            }
+                        }
+                    }
+                }
+            }
+            if (!empty($oppositeUsers)) {
+                $file = fopen('Storage/persons.json', 'w');
+                fwrite($file, json_encode($oppositeUsers));
+                fclose($file);
+            }
         }
 
-            $PersonString = file_get_contents('Storage/persons.json');
-            $people = json_decode($PersonString, true);
-            if (!empty($people)) {
-                if (isset($_POST['like'])) {
+        $PersonString = file_get_contents('Storage/persons.json');
+        $people = json_decode($PersonString, true);
+        if (!empty($people)) {
+            if (isset($_POST['like'])) {
 
-                    $likedPerson = new LikedPerson($userEmail, $people[0]);
-                    $likedPerson = get_object_vars($likedPerson);
+                $likedPerson = new LikedPerson($userEmail, $people[0]);
+                $likedPerson = get_object_vars($likedPerson);
 
-                    if (!empty($likedPersonString)) {
+                if (!empty($likedPersonString)) {
 
-                        $fileToEncode = array_merge($likedPersonArray, [$likedPerson]);
-                    } else {
-                        $fileToEncode = [$likedPerson];
-                    }
-                    $file = fopen('Storage/likedPersons.json', 'w');
-                    fwrite($file, json_encode($fileToEncode));
-                    fclose($file);
-                    unset($people[0]);
-                    $file = fopen('Storage/persons.json', 'w');
-                    $people = array_values($people);
-                    fwrite($file, json_encode($people));
-                    fclose($file);
+                    $fileToEncode = array_merge($likedPersonArray, [$likedPerson]);
+                } else {
+                    $fileToEncode = [$likedPerson];
+                }
+                $file = fopen('Storage/likedPersons.json', 'w');
+                fwrite($file, json_encode($fileToEncode));
+                fclose($file);
+                unset($people[0]);
+                $file = fopen('Storage/persons.json', 'w');
+                $people = array_values($people);
+                fwrite($file, json_encode($people));
+                fclose($file);
                 header('Location: findPeople');
-                }
-                if (isset($_POST['dislike'])) {
-                    $dislikedPersonString = file_get_contents('Storage/dislikedPersons.json');
-                    $dislikedPerson = new DislikedPerson($userEmail, $people[0]);
-                    if (!empty($dislikedPersonString)) {
-                        $dislikedPersonArray = json_decode($dislikedPersonString, true);
-                        $fileToEncode = array_merge($dislikedPersonArray, [$dislikedPerson]);
-                    } else {
-                        $fileToEncode = [$dislikedPerson];
-                    }
-                    $file = fopen('Storage/dislikedPersons.json', 'w');
-                    fwrite($file, json_encode($fileToEncode));
-                    fclose($file);
-                    unset($people[0]);
-                    $file = fopen('Storage/persons.json', 'w');
-                    $people = array_values($people);
-                    fwrite($file, json_encode($people));
-                    fclose($file);
-                    header('Location: findPeople');
-                }
-                $error = '';
-            } else {
-                $people[0]['picture_path'] = 'default.png';
-                $error = 'No more people to show';
             }
+            if (isset($_POST['dislike'])) {
+
+                $dislikedPerson = new DislikedPerson($userEmail, $people[0]);
+                $dislikedPerson = get_object_vars($dislikedPerson);
+
+                if (!empty($dislikedPersonString)) {
+
+                    $fileToEncode = array_merge($dislikedPersonArray, [$dislikedPerson]);
+                } else {
+                    $fileToEncode = [$dislikedPerson];
+                }
+                $file = fopen('Storage/dislikedPersons.json', 'w');
+                fwrite($file, json_encode($fileToEncode));
+                fclose($file);
+                unset($people[0]);
+                $file = fopen('Storage/persons.json', 'w');
+                $people = array_values($people);
+                fwrite($file, json_encode($people));
+                fclose($file);
+                header('Location: findPeople');
+            }
+            $error = '';
+        } else {
+            $people[0]['picture_path'] = 'default.png';
+            $error = 'No more people to show';
+        }
         $this->context = [
             'date' => $date,
             'person' => $people[0],
